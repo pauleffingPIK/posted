49b3b11f4f058a7b7693770b798c46dfee9671f7
pauleffingPIK <118884019+pauleffingPIK@users.noreply.github.com>
2023-08-17 05:13:51 +0400

    Update exports.R

--- R/exports.R
+++ R/exports.R
@@ -4,5 +4,9 @@ source("R/config/config.R")
 source("R/ted/TEBase.R")
 source("R/ted/TEDataFile.R")
 source("R/ted/TEDataSet.R")
+#' @export TEBase
+TEBase <- TEBase
+#' @export TEDataFile
+TEDataFile <- TEDataFile
 #' @export TEDataSet
 TEDataSet <- TEDataSet
